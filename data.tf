d73bfd512546fe58c72483aaebeeb4a845d11fdc
Vansh Madan <31353777+vanshmadan@users.noreply.github.com>
2025-05-24 23:37:01 +0530

    Update data.tf

--- data.tf
+++ data.tf
@@ -2,8 +2,14 @@ data "external" "active_droplet" {
   program = [
     "bash",
     "${path.module}/scripts/fetch_active_droplet.sh",
-    digitalocean_floating_ip.app_ip.id,
-    digitalocean_droplet.blue.id
+    digitalocean_floating_ip.app_ip.ip,
+    digitalocean_droplet.blue.id,
+    digitalocean_droplet.green.id
   ]
-  depends_on = [digitalocean_floating_ip.app_ip, digitalocean_droplet.blue]
+
+  depends_on = [
+    digitalocean_floating_ip.app_ip,
+    digitalocean_droplet.blue,
+    digitalocean_droplet.green
+  ]
 }
